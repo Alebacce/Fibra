d2827681342d243bc976296dab63ad7a2d7e4cc7
Alessandro Baccelli <a.baccelli@wikicasa.it>
2023-10-01 00:55:54 +0200

    Hello world 🏴‍☠️

---
+++ LosPiratasDoFutbolWebApplication.java
@@ -0,0 +1,10 @@
+public class LosPiratasDoFutbolWebApplication {
+    public static void main(String args[]) {
+        try {
+            System.out.println("Ciao sono l'app dei Los Piratas do Fùtbol!!");
+
+        } catch (Exception e) {
+            System.err.println(e.getMessage() + " " + e.getCause());    
+        }
+    }
+}
